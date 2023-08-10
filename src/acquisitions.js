5a1ed0af701cf952a3abf4e659414c6e4ad6f460
Nicola Casarin <nico.casarin@icloud.com>
2023-08-10 03:12:29 +0200

    adding d3js

--- src/acquisitions.js
+++ src/acquisitions.js
@@ -1,48 +1,95 @@
-
-// Set Dimensions
-const xSize = 500;
-const ySize = 500;
-const margin = 40;
-const xMax = xSize - margin * 2;
-const yMax = ySize - margin * 2;
-
-// Create Random Points
-const numPoints = 42;
-const data = [];
-for (let i = 0; i < numPoints; i++) {
-    data.push([Math.random() * xMax, Math.random() * yMax]);
-}
-
-// Append SVG Object to the Page
-const svg = d3.select("#myPlot")
-    .append("svg")
-    .append("g")
-    .attr("transform", "translate(" + margin + "," + margin + ")");
-
-// X Axis
-const x = d3.scaleLinear()
-    .domain([0, 500])
-    .range([0, xMax]);
-
-svg.append("g")
-    .attr("transform", "translate(0," + yMax + ")")
-    .call(d3.axisBottom(x));
-
-// Y Axis
-const y = d3.scaleLinear()
-    .domain([0, 500])
-    .range([yMax, 0]);
-
-svg.append("g")
-    .call(d3.axisLeft(y));
-
-// Dots
-svg.append('g')
-    .selectAll("dot")
-    .data(data)
-    .enter()
-    .append("circle")
-        .attr("cx", function (d) { return d[0] })
-        .attr("cy", function (d) { return d[1] })
-        .attr("r", 4)
-        .style("fill", "Purple");
+{
+    // Specify the dimensions of the chart.
+    var data = d3.json("/src/graph.json");
+    const width = 928;
+    const height = 680;
+  
+    // Specify the color scale.
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
+  
+    // The force simulation mutates links and nodes, so create a copy
+    // so that re-evaluating this cell produces the same result.
+    const links = data.links.map(d => ({...d}));
+    const nodes = data.nodes.map(d => ({...d}));
+  
+    // Create a simulation with several forces.
+    const simulation = d3.forceSimulation(nodes)
+        .force("link", d3.forceLink(links).id(d => d.id))
+        .force("charge", d3.forceManyBody())
+        .force("x", d3.forceX())
+        .force("y", d3.forceY());
+  
+    // Create the SVG container.
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [-width / 2, -height / 2, width, height])
+        .attr("style", "max-width: 100%; height: auto;");
+  
+    // Add a line for each link, and a circle for each node.
+    const link = svg.append("g")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
+      .selectAll("line")
+      .data(links)
+      .join("line")
+        .attr("stroke-width", d => Math.sqrt(d.value));
+  
+    const node = svg.append("g")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
+      .selectAll("circle")
+      .data(nodes)
+      .join("circle")
+        .attr("r", 5)
+        .attr("fill", d => color(d.group));
+  
+    node.append("title")
+        .text(d => d.id);
+  
+    // Add a drag behavior.
+    node.call(d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended));
+    
+    // Set the position attributes of links and nodes each time the simulation ticks.
+    simulation.on("tick", () => {
+      link
+          .attr("x1", d => d.source.x)
+          .attr("y1", d => d.source.y)
+          .attr("x2", d => d.target.x)
+          .attr("y2", d => d.target.y);
+  
+      node
+          .attr("cx", d => d.x)
+          .attr("cy", d => d.y);
+    });
+  
+    // Reheat the simulation when drag starts, and fix the subject position.
+    function dragstarted(event) {
+      if (!event.active) simulation.alphaTarget(0.3).restart();
+      event.subject.fx = event.subject.x;
+      event.subject.fy = event.subject.y;
+    }
+  
+    // Update the subject (dragged node) position during drag.
+    function dragged(event) {
+      event.subject.fx = event.x;
+      event.subject.fy = event.y;
+    }
+  
+    // Restore the target alpha so the simulation cools after dragging ends.
+    // Unfix the subject position now that it’s no longer being dragged.
+    function dragended(event) {
+      if (!event.active) simulation.alphaTarget(0);
+      event.subject.fx = null;
+      event.subject.fy = null;
+    }
+  
+    // When this cell is re-run, stop the previous simulation. (This doesn’t
+    // really matter since the target alpha is zero and the simulation will
+    // stop naturally, but it’s a good practice.)
+    invalidation.then(() => simulation.stop());
+  
+  }
